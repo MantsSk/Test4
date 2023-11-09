b169c5afda1a19a22d8904a98e0b9f5b40176481
MSkara <mantas.skara@gmail.com>
2023-11-09 10:15:38 +0200

    Create 3uzduotis.py

---
+++ 4PamokaKonsultacija/3uzduotis.py
@@ -0,0 +1,7 @@
+# Create a list
+my_list = [10, 20, 30, 40, 50]
+
+# Iterate through the list, add 100 to each element, and print the modified elements
+for element in my_list:
+    modified_element = element + 100
+    print(modified_element)
