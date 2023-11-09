f646d0a768591afb78ecb51db8354a6a4d34afb2
MSkara <mantas.skara@gmail.com>
2023-11-09 10:09:57 +0200

    Create 1uzduotis.py

---
+++ 4PamokaKonsultacija/1uzduotis.py
@@ -0,0 +1,17 @@
+# Allow the user to enter the first and second numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+# Perform five mathematical operations with the input numbers
+addition = first_number + second_number
+subtraction = first_number - second_number
+division = first_number / second_number
+multiplication = first_number * second_number
+square = first_number ** 2 + second_number ** 2
+
+# Print all five results to the console
+print("Sum:", addition)
+print("Difference:", subtraction)
+print("Division:", division)
+print("Multiplication:", multiplication)
+print("Square and sum:", square)
