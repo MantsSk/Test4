5af84849dec51dd7eb2d3c6fd4ee06190f0ac06b
MSkara <mantas.skara@gmail.com>
2023-11-20 15:50:51 +0200

    Create 1uzduotis.py

---
+++ 10Pamoka - Funkcijos2/1uzduotis.py
@@ -0,0 +1,21 @@
+def puzzle_pieces(list1, list2):
+    if len(list1) != len(list2):
+        return False  # Lists must have the same length
+
+    sum_equal = True
+    total_sum = list1[0] + list2[0]
+
+    for i in range(1, len(list1)):
+        current_sum = list1[i] + list2[i]
+        sum_equal = sum_equal and (current_sum == total_sum)
+
+    return sum_equal
+
+# Test cases
+print(puzzle_pieces([1, 2, 3, 4], [4, 3, 2, 1]))  # True
+# 1 + 4 = 5;  2 + 3 = 5;  3 + 2 = 5;  4 + 1 = 5
+# Both lists sum to [5, 5, 5, 5]
+
+print(puzzle_pieces([1, 8, 5, 0, -1, 7], [0, -7, -4, 1, 2, -6]))  # True
+print(puzzle_pieces([1, 2], [-1, -1]))  # False
+print(puzzle_pieces([9, 8, 7], [7, 8, 9, 10]))  # False
