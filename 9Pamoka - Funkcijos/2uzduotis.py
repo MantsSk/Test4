35285257972b95cc543adeb02648e9cdb1d89950
MSkara <mantas.skara@gmail.com>
2023-11-21 21:33:04 +0200

    Update 2uzduotis.py

--- 9Pamoka - Funkcijos/2uzduotis.py
+++ 9Pamoka - Funkcijos/2uzduotis.py
@@ -6,3 +6,23 @@ def add_ending_to_list(lst, ending):
 original_list = ["apple", "banana", "cherry"]
 result_list = add_ending_to_list(original_list, "_end")
 print(result_list)
+
+# fruit_list = ["apple", "banana", "cherry"]
+# heh_list = ["heh", "kek", "rek"]
+# people_list = ["Mantas", "Matas", "Tomas"]
+
+# def add_ending_to_list(passed_list):
+#     modified_passed_list = []
+
+#     for item in passed_list:
+#         item += "_ending"
+#         modified_passed_list.append(item)
+
+#     return modified_passed_list
+
+
+# new_list = []
+# new_list = add_ending_to_list(fruit_list)
+# print(new_list)
+
+# print(add_ending_to_list(people_list))
