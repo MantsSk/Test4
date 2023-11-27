47f0d136cf075311a357498acef58bdc8728ffe2
MSkara <mantas.skara@gmail.com>
2023-11-27 19:50:17 +0200

    Update first_task_as_script.py

---
+++ 12Pamoka - Modules/first_task/first_task_as_script.py
@@ -0,0 +1,20 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+
+
+addition = add(5,12)
+
+print(addition)
