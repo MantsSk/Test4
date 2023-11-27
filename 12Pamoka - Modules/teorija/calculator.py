024fe0bb4241c9e348b65672dd41e51cf182d979
MSkara <mantas.skara@gmail.com>
2023-11-27 19:45:11 +0200

    update

---
+++ 12Pamoka - Modules/teorija/calculator.py
@@ -0,0 +1,18 @@
+name = "Mantas"
+vardas = "Vardas"
+
+
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def prod(x, y):
+    return x * y
+
+
+def div(x, y):
+    return x // y
---
+++ 12Pamoka - Modules/teorija/main.py
@@ -0,0 +1,33 @@
+# import calculator
+# import calculator as calc
+# from calculator import *
+from calculator import add, sub, vardas
+import datetime
+
+x = datetime.datetime.now()
+
+print(x)
+
+# x = calculator.add(5,5)
+# y = calculator.sub(12,3)
+
+# x = calc.add(5,5)
+# y = calc.sub(12,3)
+
+cat = "Tom"
+
+x = add(5,5)
+y = sub(12,3)
+
+if __name__ == "__main__":
+    print("Hello world")
+
+    print(x)
+    print(y)
+
+    print(vardas)
+
+    vardas = "Mantas1000"
+
+    print(vardas)
+
