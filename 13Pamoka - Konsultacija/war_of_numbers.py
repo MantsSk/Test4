2c5e27ea771f909838942c99b57f76b54a22b731
MSkara <mantas.skara@gmail.com>
2023-11-28 16:03:24 +0200

    Update war_of_numbers.py

--- 13Pamoka - Konsultacija/war_of_numbers.py
+++ 13Pamoka - Konsultacija/war_of_numbers.py
@@ -1,14 +1,18 @@
 def war_of_numbers(numbers):
     even_sum = sum(num for num in numbers if num % 2 == 0)
     odd_sum = sum(num for num in numbers if num % 2 != 0)
-    return abs(even_sum - odd_sum)
 
-# Test cases
-print(war_of_numbers([2, 8, 7, 5]))  # 2
-# 2 + 8 = 10
-# 7 + 5 = 12
-# 12 is larger than 10
-# So we return 12 - 10 = 2
+    difference = even_sum - odd_sum
 
-print(war_of_numbers([12, 90, 75]))  # 27
-print(war_of_numbers([5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243]))  # 168
+    if difference > 0:
+        print("Evens win by", difference)
+    elif difference < 0:
+        print("Odds win by", abs(difference))
+    else:
+        print("It's a tie!")
+
+
+# Example usage:
+numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+numbers_list2 = [1,4,4,2,5,4]
+war_of_numbers(numbers_list2)
