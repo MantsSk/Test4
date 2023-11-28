489c1738b6d73cc2860f3d8aaffd115b13945ce9
MSkara <mantas.skara@gmail.com>
2023-11-28 13:25:10 +0200

    Create bigrams.py

---
+++ 13Pamoka - Konsultacija/bigrams.py
@@ -0,0 +1,28 @@
+def can_find(bigrams, words):
+    for bigram in bigrams:
+        found = False
+        for word in words:
+            if bigram in word:
+                found = True
+                break
+        if not found:
+            return False
+    return True
+
+# or
+
+# def can_find(bigrams, words):
+#     for bigram in bigrams:
+#         found = any(bigram in word for word in words)
+#         if not found:
+#             return False
+#     return True
+
+# Test cases
+print(can_find(["at", "be", "th", "au"], ["beautiful", "the", "hat"]))  # True
+print(can_find(["ay", "be", "ta", "cu"], ["maybe", "beta", "abet", "course"]))  # False
+# "cu" does not exist in any of the words.
+print(can_find(["th", "fo", "ma", "or"], ["the", "many", "for", "forest"]))  # True
+print(can_find(["oo", "mi", "ki", "la"], ["milk", "chocolate", "cooks"]))  # False
+
+
